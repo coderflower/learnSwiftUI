b83e305b923c78ef55ca640c2f81fa5741d54855
蔡龙君 <cailongjun@huami.com>
2019-06-12 14:45:19 +0800

    feat |> 添加Drawing target

---
+++ DrawingAndAnimation/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DrawingAndAnimation
+//
+//  Created by 蔡龙君 on 2019/6/12.
+//  Copyright © 2019 蔡龙君. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
