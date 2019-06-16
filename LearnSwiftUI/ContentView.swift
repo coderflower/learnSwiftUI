a231a7eb30b6d0c07857db53f831c882295d0beb
蔡龙君 <cailongjun@huami.com>
2019-06-16 22:01:36 +0800

    feat |> How to add bar items to a navigation view

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -4,13 +4,22 @@
 //
 //  Created by 蔡龙君 on 2019/6/12.
 //  Copyright © 2019 蔡龙君. All rights reserved.
-//
+//  https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-bar-items-to-a-navigation-view
 
 import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Text("This is a gerat app")
+             /// 默认情况下 导航栏都是largeTitle,
+            .navigationBarTitle(Text("Welcome"), displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: {
+                    print("Help tapped!")
+                }, label: {
+                    Text("Help")
+                }))
+        }
     }
 }
 
